13a5a17a5ba3dca65843d91f6f387f65c6295d21
Duncan Home <iampunkrock@gmail.com>
2014-09-29 22:32:53 +0100

    Revert "move test"
    This reverts commit 66e557e619d6d35e65f1d0b8d197cee6e7b60bd6.

--- include/hypobase.h
+++ include/hypobase.h
@@ -3,44 +3,6 @@
 #define HYPOBASE_H
 
 
-/*
-#ifdef _DEBUG
-      inline void * __cdecl operator new(unsigned int size, 
-                                         const char *file, int line)
-      {
-      };
-
-      inline void __cdecl operator delete(void *p)
-      {
-      };
-#endif
-
-#ifdef _DEBUG
-      #define DEBUG_NEW new(__FILE__, __LINE__)
-      #else
-      #define DEBUG_NEW new
-      #endif
-      #define new DEBUG_NEW
-
-
-
-      #ifdef _DEBUG
-      inline void * __cdecl operator new(unsigned int size,
-                                         const char *file, int line)
-      {
-	      void *ptr = (void *)malloc(size);
-	      AddTrack((DWORD)ptr, size, file, line);
-	      return(ptr);
-      };
-      inline void __cdecl operator delete(void *p)
-      {
-	      RemoveTrack((DWORD)p);
-	      free(p);
-      };
-      #endif
- */
-
-
 #include "wx/wx.h"
 
 #include "wx/spinctrl.h"
@@ -68,7 +30,7 @@
 //#define _CRTDBG_MAP_ALLOC_NEW
 //#include <stdlib.h>
 //#include <crtdbg.h>
-//#include <vld.h>
+#include <vld.h>
 
 
 //using namespace stk;
