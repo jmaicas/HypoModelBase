afd5911977f96cb42d75680d22a21b286897824d
Duncan Home <iampunkrock@gmail.com>
2014-09-29 22:37:44 +0100

    include cleanup

--- include/hypobase.h
+++ include/hypobase.h
@@ -36,50 +36,6 @@
 //using namespace stk;
 
 
-/*
-typedef struct {
-	      DWORD	address;
-	      DWORD	size;
-	      char	file[64];
-	      DWORD	line;
-      } ALLOC_INFO;
-
-      typedef list<ALLOC_INFO*> AllocList;
-
-      AllocList *allocList;
-
-      void AddTrack(DWORD addr,  DWORD asize,  const char *fname, DWORD lnum)
-      {
-	      ALLOC_INFO *info;
-
-	      if(!allocList) {
-		      allocList = new(AllocList);
-	      }
-
-	      info = new(ALLOC_INFO);
-	      info->address = addr;
-	      strncpy(info->file, fname, 63);
-	      info->line = lnum;
-	      info->size = asize;
-	      allocList->insert(allocList->begin(), info);
-      };
-
-      void RemoveTrack(DWORD addr)
-      {
-	      AllocList::iterator i;
-
-	      if(!allocList)
-		      return;
-	      for(i = allocList->begin(); i != allocList->end(); i++)
-	      {
-		      if((*i)->address == addr)
-		      {
-			      allocList->remove((*i));
-			      break;
-		      }
-	      }
-      };
-*/
 
 enum {
 	spincon = 1,
